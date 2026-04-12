723d44eb621adabae241806d4a5c6097a0cfbc9a
niduranga <nidurangajayarathna@gmail.com>
2026-04-12 22:09:56 +0530

    Fix: asset base path for github pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [tailwindcss(),react()],
-  base: '/niduranga_jayarathna/',
+  // base: '/niduranga_jayarathna/',
 })
